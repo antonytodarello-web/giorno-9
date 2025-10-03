d0085d1b52faab781be44d7515f2c6857e5e0e88
Antonio Todarello-web <antonytodarello@gmail.com>
2025-10-03 16:52:21 +0200

    agg

--- D5.js
+++ D5.js
@@ -71,14 +71,24 @@ const justTrims = [];
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
-
+cars.forEach((car) => {
+  if (car.color.charAt(0) === "b") {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+});
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
-
+let counter = numericArray;
+while (counter === 32) {
+  counter = counter + 1;
+  console.log("ecco il numero:", numericArray);
+}
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
